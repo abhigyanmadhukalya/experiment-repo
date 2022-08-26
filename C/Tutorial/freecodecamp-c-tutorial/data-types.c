0caa3c4ceb901b54b87e84d8ed85a479df643b88
Abhigyan Madhukalya <102839682+abhigyanmadhukalya@users.noreply.github.com>
2022-08-26 19:54:28 +0530

    Learnin C

---
+++ C/Tutorial/freecodecamp-c-tutorial/data-types.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  // Numbers
+  int age = 40;
+  double gpa = 3.7;
+
+  // Characthers
+  char grade = 'A';
+  char name[] = "Abhigyan";
+
+  return 0;
+}
---
+++ C/Tutorial/freecodecamp-c-tutorial/printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  int favNum = 69;
+  char favLetter = 'A';
+
+  printf("Hello world\n");
+  printf("Integer: %d\n", 500);
+  printf("Double: %f\n", 6.9);
+  printf("String: %s\n", "Abhigyan");
+  printf("Using variable favNum: %d\n", favNum);
+  printf("Using variable favLetter: %c\n", favLetter);
+
+  return 0;
+}
