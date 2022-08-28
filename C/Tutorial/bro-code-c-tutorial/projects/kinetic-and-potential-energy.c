4448c1de8e33789f21da4a68a36c6d58022e57f6
Abhigyan Madhukalya <102839682+abhigyanmadhukalya@users.noreply.github.com>
2022-08-28 20:46:19 +0530

    Made changes to kinectic-energy.c

---
+++ C/Tutorial/bro-code-c-tutorial/projects/kinetic-and-potential-energy.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+
+  double mass;
+  double velocity;
+  double kinetic_energy;
+  double potential_energy;
+  double height;
+  char energy;
+  const double G = 9.8;
+
+  printf(
+      "Do you want to calculate Potential Energy(P) or Kinectic Energy(K): ");
+  scanf("%c", &energy);
+
+  switch (energy) {
+  case 'K':
+    printf("Mass of the given body(in kg): ");
+    scanf("%lf", &mass);
+
+    printf("Velocity of the given body(in m/s): ");
+    scanf("%lf", &velocity);
+
+    kinetic_energy = mass * velocity / 2;
+    printf("\nThe Kinectic Energy of the given body is: %lfJ", kinetic_energy);
+    break;
+  case 'P':
+    printf("Mass of the given body: ");
+    scanf("%lf", &mass);
+
+    printf("Height: ");
+    scanf("%lf", &height);
+
+    potential_energy = mass * height * G;
+    printf("\nThe Potential Energy of the given body is: %lfJ",
+           potential_energy);
+    break;
+  default:
+    printf("%c is not a valid response", &energy);
+  }
+
+  return 0;
+}
