3540e1e8f61162c62aa56a7b258b8aa6ef073306
Abhigyan Madhukalya <102839682+abhigyanmadhukalya@users.noreply.github.com>
2023-01-11 20:59:16 +0530

    messing around with Javascript

---
+++ Javascript/JS/arrays.js
@@ -0,0 +1,24 @@
+// Example
+let students = ["Abhigyan", "Derek", "James", "Patrick"];
+
+// Nested Array
+let school = [
+    ["Narayana", 0],
+    ["Sanskriti The Gurukul", 1],
+];
+
+// Accessing data in an array
+student1 = students[0];
+student2 = students[1];
+
+// Accessing Multi-dimensional arrays
+school1 = school[0][0];
+school2 = school[1][0];
+
+// Adding data to an array
+students.push("John"); // Adds an element to the end of an array
+students.unshift("Jane"); // Adds an element to the beginning of an array
+
+// Removing data from an array
+students.pop(); // Removes from the end of an array
+students.shift(); // Removes from the beginning of an array
--- Javascript/JS/functions.js
+++ Javascript/JS/functions.js
@@ -1,35 +1,40 @@
 function printTax(price) {
-  let tax = price * 0.21
-  console.log("tax: ${tax}")
+    let tax = price * 0.21;
+    console.log("tax: ${tax}");
 }
 
-printTax(200)
+printTax(200);
 //-----------------------------------------
 
 function calculateTax(price) {
-  return price * 0.21
+    return price * 0.21;
 }
 
-const result = calculateTax(100)
-console.log(result)
+const result = calculateTax(100);
+console.log(result);
 
 // Reusing the function using variables
 
-let bread = 100
-let milk = 50
+let bread = 100;
+let milk = 50;
 
-breadTax = calculateTax(bread)
-milkTax = calculateTax(milk)
+breadTax = calculateTax(bread);
+milkTax = calculateTax(milk);
 
-console.log("Tax for bread is ${breadTax}")
-console.log("Tax for milk is ${milkTax}")
+console.log("Tax for bread is ${breadTax}");
+console.log("Tax for milk is ${milkTax}");
 //-----------------------------------------
 
 // Multiple Parameters
 
 function displayInfo(name, age, country) {
-  console.log("${name} is ${age} years old and from ${country}")
+    console.log("${name} is ${age} years old and from ${country}");
 }
 
-displayInfo('Abhigyan', 17, 'India')
+displayInfo("Abhigyan", 17, "India");
 //------------------------------------------
+
+// Return Statement
+function returnInfo(name, age, country) {
+    return "${name} is ${age} years old and from ${country}";
+}
