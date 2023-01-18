c4c3f3f2ac481bb2a164f623d1491948e8bcc83d
Abhigyan Madhukalya <102839682+abhigyanmadhukalya@users.noreply.github.com>
2023-01-18 20:12:07 +0530

    started learning c++ for the second time

---
+++ C++/first-program/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world" << std::endl;       // std::endl writes the output in a new line
+    std::cout << "This is my first C++ program. "; // This will not make a new line
+    std::cout << "This line is on the same line as the previous line" << std::endl;
+    return 0;
+}
