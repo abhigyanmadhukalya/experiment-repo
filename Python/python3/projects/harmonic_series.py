e79cd946d93e425111d869ebf05aa98a54c78a3e
Abhigyan Madhukalya <102839682+abhigyanmadhukalya@users.noreply.github.com>
2023-01-22 15:27:02 +0530

    made the code smaller for harmonic series

--- Python/python3/projects/harmonic_series.py
+++ Python/python3/projects/harmonic_series.py
@@ -1,12 +1,7 @@
 from math import fsum
+from fractions import Fraction
 
 n = int(input("Enter range of 'n': "))
-Numbers = []
+numbers = [Fraction(1, i * i * i) for i in range(1, n + 1)]
 
-for i in range(1, (n + 1)):
-    numbers = f"1/(i*i*i)"
-    Numbers.append(numbers)
-
-Int_Numbers = [eval(i) for i in Numbers]
-total_sum = fsum(Int_Numbers)
-print(f"The sum of the given harmonic series is {total_sum}")
+print(f"The sum of the given harmonic series is {fsum(numbers):.4f}")
